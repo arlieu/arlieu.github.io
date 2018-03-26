d55599df6a58e5ce1111b2fdf92a642320af62e0
Avery Lieu <avelieu@gmail.com>
2018-03-25 20:59:14 -0500

    new sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,14 +1,28 @@
-$(function(){
+$(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
 
-    $('#slide-submenu').on('click',function() {
-        $(this).closest('.list-group').fadeOut('slide',function(){
-        	$('.mini-submenu').fadeIn();
-        });
+    trigger.click(function () {
+      hamburger_cross();
+    });
 
-      });
+    function hamburger_cross() {
 
-	$('.mini-submenu').on('click',function(){
-        $(this).next('.list-group').toggle('slide');
-        $('.mini-submenu').hide();
-	})
-})
+      if (isClosed == true) {
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });
+});
