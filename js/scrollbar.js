ee3186721168dff2769153fe8e0e009b5d43a8f8
arlieu <avelieu@gmail.com>
2018-04-07 08:48:36 -0500

    scrollify auto execute

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,18 +1,20 @@
-$.scrollify({
-    section : "section",
-    sectionName : "section-name",
-    interstitialSection : "",
-    easing: "easeOutExpo",
-    scrollSpeed: 1500,
-    offset : 0,
-    scrollbars: true,
-    standardScrollElements: "",
-    setHeights: true,
-    overflowScroll: true,
-    updateHash: true,
-    touchScroll:true,
-    before:function() {},
-    after:function() {},
-    afterResize:function() {},
-    afterRender:function() {}
-  });
+$(function() {
+  $.scrollify({
+      section : "section",
+      sectionName : "section-name",
+      interstitialSection : "",
+      easing: "easeOutExpo",
+      scrollSpeed: 1500,
+      offset : 0,
+      scrollbars: true,
+      standardScrollElements: "",
+      setHeights: true,
+      overflowScroll: true,
+      updateHash: true,
+      touchScroll:true,
+      before:function() {},
+      after:function() {},
+      afterResize:function() {},
+      afterRender:function() {}
+    });
+});
