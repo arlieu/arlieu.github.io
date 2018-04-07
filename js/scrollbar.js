4eeab4a0087966077b790eb2382294d3df94920c
arlieu <avelieu@gmail.com>
2018-04-06 23:50:18 -0500

    scroll bar animation

---
+++ js/scrollbar.js
@@ -0,0 +1,18 @@
+$.scrollify({
+    section : "section",
+    sectionName : "section-name",
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1500,
+    offset : 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll:true,
+    before:function() {},
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {}
+  });
