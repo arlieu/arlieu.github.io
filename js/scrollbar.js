23225d9f6189820aac81aefbac273ac1f2ab30dc
arlieu <avelieu@gmail.com>
2018-04-07 09:16:41 -0500

    fullPage.js scrolling

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,20 +1,3 @@
-$(function() {
-  $.scrollify({
-      section : "section",
-      sectionName : "section-name",
-      interstitialSection : "",
-      easing: "easeOutExpo",
-      scrollSpeed: 1500,
-      offset : 0,
-      scrollbars: true,
-      standardScrollElements: "",
-      setHeights: true,
-      overflowScroll: true,
-      updateHash: true,
-      touchScroll:true,
-      before:function() {},
-      after:function() {},
-      afterResize:function() {},
-      afterRender:function() {}
-    });
+$(document).ready(function() {
+	$('#fullpage').fullpage();
 });
