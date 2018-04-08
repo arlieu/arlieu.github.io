d6cdefee44663f41a30b9e6c9eadc5afb4ca7391
arlieu <avelieu@gmail.com>
2018-04-08 13:15:07 -0500

    debugging fullPage.js issues

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-	$('#fullpage').fullpage();
+	$('#fullpage').fullpage({
+    scrollBar: false,
+    scrollingSpeed: 700,
+    autoScrolling: true,
+    fitToSection: true,
+    loopBottom: true,
+    loopTop: true
+  });
+  alert("HTML Class List: " + document.documentElement.className.split(' '))
 });
