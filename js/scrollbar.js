0ac631399a0806ebef0b2da72398559b5f77e13c
arlieu <avelieu@gmail.com>
2018-04-08 13:26:31 -0500

    force adding classes to html and body

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
+  document.documentElement.classList.add('fp-enabled');
+  document.body.classList.add('fp-responsive');
 	$('#fullpage').fullpage({
-    scrollBar: false,
+    scrollBar: true,
     scrollingSpeed: 700,
     autoScrolling: true,
     fitToSection: true,
     loopBottom: true,
     loopTop: true
   });
-  alert("HTML Class List: " + document.documentElement.className.split(' '))
+  alert("HTML Class List: " + document.documentElement.className.split(' '));
+  alert("Body Class List: " + document.body.className.split(' '))
 });
