f994e3fa720601b6f0e75da749ba16f3ccb43260
arlieu <avelieu@gmail.com>
2018-04-08 13:59:02 -0500

    testing data-anchors

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
-  document.documentElement.classList.add('fp-enabled');
-  document.body.classList.add('fp-responsive');
 	$('#fullpage').fullpage({
     scrollBar: true,
     scrollingSpeed: 700,
     autoScrolling: true,
     fitToSection: true,
     loopBottom: true,
-    loopTop: true
+    loopTop: true,
+    anchors: ['main', 'work-experience']
   });
-  alert("HTML Class List: " + document.documentElement.className.split(' '));
-  alert("Body Class List: " + document.body.className.split(' '))
 });
