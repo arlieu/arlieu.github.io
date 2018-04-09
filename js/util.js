7bd42910741c5020fbad6cf23569af86805dece0
arlieu <avelieu@gmail.com>
2018-04-08 21:44:57 -0500

    remove sizing

--- js/util.js
+++ js/util.js
@@ -1,8 +1,8 @@
 $('.skill-logo').hover(
   function() {
-    $(this).animate({ opacity: .4, width: '85%' }, 300);
+    $(this).fadeTo(250, .5);
   },
   function() {
-    $(this).animate({ opacity: 1, width: '75%' }, 300);
+    $(this).fadeTo(400, 1);
   }
 );
